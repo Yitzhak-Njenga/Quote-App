4edceb522da8528f14e67e671230af0a7a71929b
Yitzhak-Njenga <yitzhaknjenga@gmail.com>
2020-11-09 23:53:38 +0300

    added new string

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,9 +10,7 @@ export class QuoteComponent implements OnInit {
   quotes:Quote[]=[
     new Quote(1, 'Don’t let the noise of others’ opinions drown out your own inner voice.', 'Author: Steve Jobs',new Date(2003,3,14)),
     new Quote(2, 'Be faithful in small things because it is in them that your strength lies', 'Author: Martha Teresa',new Date(1972,7,9)),
-    new Quote(3, 'Success is determined not by whether or not you face obstacles, but by your reaction to them. ', 'Author: Dr. Ben carson',new Date(1987,0,12)),
-    new Quote(4, 'Genius is one percent inspiration and ninety-nine percent perspiration', 'Author: By Thomas Edison',new Date(2019,6,18)),
-    ];
+     ];
 
   upVote(index) {
 
